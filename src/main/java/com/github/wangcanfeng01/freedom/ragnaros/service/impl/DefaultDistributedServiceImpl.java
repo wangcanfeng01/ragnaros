b7efa7c3121ed2e179ea55ab1b46cbca746ae11c
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2020-04-11 11:38:23 +0800

    Update DefaultDistributedServiceImpl.java

--- src/main/java/com/github/wangcanfeng01/freedom/ragnaros/service/impl/DefaultDistributedServiceImpl.java
+++ src/main/java/com/github/wangcanfeng01/freedom/ragnaros/service/impl/DefaultDistributedServiceImpl.java
@@ -10,22 +10,23 @@ import java.util.List;
 import static com.github.wangcanfeng01.freedom.ragnaros.constant.RagnarosConsts.DEFAULT_DISTRIBUTED_SERVICE;
 
 /**
+ * 分布式接口实现类
+ * Created in 17:03-2020/4/7
+ *
  * @author wangcanfeng
- * @description 分布式接口实现类
- * @date Created in 17:03-2020/4/7
- * @since 2.0.0
+ * @since 1.0.0
  */
 @Service(DEFAULT_DISTRIBUTED_SERVICE)
 public class DefaultDistributedServiceImpl extends AbstractDistributedService {
 
     /**
      * 功能描述: 获取其他服务实例的地址
+     * 2020/4/7-17:08
      *
      * @param localhost 本实例的服务地址
      * @return 返回信息：
      * @author wangcanfeng
-     * @date 2020/4/7-17:08
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public List<String> otherServiceAddress(String localhost) {
@@ -34,12 +35,12 @@ public class DefaultDistributedServiceImpl extends AbstractDistributedService {
 
     /**
      * 功能描述: 实例地址对应的吞吐量信息
+     * 2020/4/7-17:09
      *
      * @param serviceAddress 需要请求的实例地址
      * @return 返回信息：实例地址对应的吞吐量信息
      * @author wangcanfeng
-     * @date 2020/4/7-17:09
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public ServiceThroughput obtainThroughput(String serviceAddress) {
@@ -48,11 +49,11 @@ public class DefaultDistributedServiceImpl extends AbstractDistributedService {
 
     /**
      * 功能描述:打开实例地址对应的吞吐量检测器
+     * 2020/4/7-17:09
      *
      * @param serviceAddress 需要请求的实例地址
      * @author wangcanfeng
-     * @date 2020/4/7-17:09
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public void openThroughput(String serviceAddress) {
@@ -60,11 +61,11 @@ public class DefaultDistributedServiceImpl extends AbstractDistributedService {
 
     /**
      * 功能描述:关闭实例地址对应的吞吐量检测器
+     * 2020/4/7-17:09
      *
      * @param serviceAddress 需要请求的实例地址
      * @author wangcanfeng
-     * @date 2020/4/7-17:09
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public void closeThroughput(String serviceAddress) {
