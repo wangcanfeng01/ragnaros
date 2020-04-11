a0b8e60c3f6adfc566466ccb41ee6eb8237f5e2d
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2020-04-11 10:54:11 +0800

    Update Throughput.java

--- src/main/java/com/github/wangcanfeng01/freedom/ragnaros/annotations/Throughput.java
+++ src/main/java/com/github/wangcanfeng01/freedom/ragnaros/annotations/Throughput.java
@@ -3,12 +3,13 @@ package com.github.wangcanfeng01.freedom.ragnaros.annotations;
 import java.lang.annotation.*;
 
 /**
+ * 加在方法上表示要围绕这个方法计算吞吐量
+ * Created in 11:51-2020/4/7
+ *
  * @author wangcanfeng
- * @description 加在方法上表示要围绕这个方法计算吞吐量
- * @date Created in 11:51-2020/4/7
  * 为了方便找到被标记的方法，还需要再类上加下面这个注释
  * @see ThroughputScan
- * @since 2.0.0
+ * @since 1.0.0
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
