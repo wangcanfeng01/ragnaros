068a18aef1a29a471d93ff55f72b4a5f88fd78b8
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2020-04-11 11:39:56 +0800

    Update AbstractDistributedService.java

--- src/main/java/com/github/wangcanfeng01/freedom/ragnaros/service/AbstractDistributedService.java
+++ src/main/java/com/github/wangcanfeng01/freedom/ragnaros/service/AbstractDistributedService.java
@@ -8,63 +8,64 @@ import java.util.Collections;
 import java.util.List;
 
 /**
+ * 分布式的抽象接口
+ * Created in 16:31-2020/4/7
+ *
  * @author wangcanfeng
- * @description 分布式的抽象接口
- * @date Created in 16:31-2020/4/7
- * @since 2.0.0
+ * @since 1.0.0
  */
 public abstract class AbstractDistributedService implements DistributedService {
 
     /**
      * 功能描述: 获取其他服务实例的地址
+     * 2020/4/7-17:08
      *
      * @param localhost 本实例的服务地址
      * @return 返回信息：实例地址集合
      * @author wangcanfeng
-     * @date 2020/4/7-17:08
-     * @since 2.0.0
+     * @since 1.0.0
      */
     public abstract List<String> otherServiceAddress(String localhost);
 
     /**
      * 功能描述: 实例地址对应的吞吐量信息
+     * 2020/4/7-17:09
      *
      * @param serviceAddress 需要请求的实例地址
      * @return 返回信息：实例地址对应的吞吐量信息
      * @author wangcanfeng
-     * @date 2020/4/7-17:09
-     * @since 2.0.0
+     * @since 1.0.0
      */
     public abstract ServiceThroughput obtainThroughput(String serviceAddress);
 
     /**
      * 功能描述:打开实例地址对应的吞吐量检测器
+     * 2020/4/7-17:09
      *
      * @param serviceAddress 需要请求的实例地址
      * @author wangcanfeng
-     * @date 2020/4/7-17:09
-     * @since 2.0.0
+     * @since 1.0.0
      */
     public abstract void openThroughput(String serviceAddress);
 
     /**
      * 功能描述:关闭实例地址对应的吞吐量检测器
+     * 2020/4/7-17:09
      *
      * @param serviceAddress 需要请求的实例地址
      * @author wangcanfeng
-     * @date 2020/4/7-17:09
-     * @since 2.0.0
+     * @since 1.0.0
      */
     public abstract void closeThroughput(String serviceAddress);
 
     /**
      * 功能描述: 观察其他实例的情况
+     * 2020/4/7-17:03
      *
      * @param localhost 当前服务器的地址
      * @return 返回信息：其他实例的吞吐量信息
      * @author wangcanfeng
-     * @date 2020/4/7-17:03
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public List<ServiceThroughput> watchOthers(String localhost) {
@@ -79,11 +80,11 @@ public abstract class AbstractDistributedService implements DistributedService {
 
     /**
      * 功能描述: 打开其他实例的观测器
+     * 2020/4/7-17:03
      *
      * @param localhost 当前服务器的地址
      * @author wangcanfeng
-     * @date 2020/4/7-17:03
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public void openOthers(String localhost) {
@@ -96,11 +97,11 @@ public abstract class AbstractDistributedService implements DistributedService {
 
     /**
      * 功能描述: 关闭其他实例的观测器
+     * 2020/4/7-17:03
      *
      * @param localhost 当前服务器的地址
      * @author wangcanfeng
-     * @date 2020/4/7-17:03
-     * @since 2.0.0
+     * @since 1.0.0
      */
     @Override
     public void closeOthers(String localhost) {
