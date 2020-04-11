d746ae842e5fef262bba5a224d2d8ed82576873a
wangcanfeng01 <32534120+wangcanfeng01@users.noreply.github.com>
2020-04-11 11:25:06 +0800

    Update ManagementService.java

--- src/main/java/com/github/wangcanfeng01/freedom/ragnaros/calculator/ManagementService.java
+++ src/main/java/com/github/wangcanfeng01/freedom/ragnaros/calculator/ManagementService.java
@@ -3,61 +3,59 @@ package com.github.wangcanfeng01.freedom.ragnaros.calculator;
 import com.github.wangcanfeng01.freedom.ragnaros.vo.ServiceThroughput;
 
 /**
+ * Created in 20:42-2020/4/7
+ *
  * @author wangcanfeng
- * @description
- * @date Created in 20:42-2020/4/7
- * @since 2.0.0
+ * @since 1.0.0
  */
 public interface ManagementService {
 
 
     /**
      * 功能描述: 获取所有需要要观测的管道信息
-     *
-     * @param
+     * 2020/4/2-9:45
      * @return 返回信息： 管道检测信息
      * @author wangcanfeng
-     * @date 2020/4/2-9:45
-     * @since 2.0.0
+     * @since 1.0.0
      */
     ServiceThroughput watch();
 
     /**
      * 功能描述: 打开监测管理器
+     * 2020/4/7-20:44
      *
      * @author wangcanfeng
-     * @date 2020/4/7-20:44
-     * @since 2.0.0
+     * @since 1.0.0
      */
     void open();
 
     /**
      * 功能描述: 关闭监测管理器
+     * 2020/4/7-20:44
      *
      * @author wangcanfeng
-     * @date 2020/4/7-20:44
-     * @since 2.0.0
+     * @since 1.0.0
      */
     void close();
 
     /**
      * 功能描述: 设置花费时间
+     * 2020/4/7-20:44
      *
      * @param name        吞吐量项目名称
      * @param currentCost 当前次处理花费
      * @author wangcanfeng
-     * @date 2020/4/7-20:44
-     * @since 2.0.0
+     * @since 1.0.0
      */
     void setCost(String name, long currentCost);
 
-    /**
+     /**
      * 功能描述: 增加吞吐量计数
+     * 2020/4/7-20:44
      *
      * @param name 吞吐量项目名称
      * @author wangcanfeng
-     * @date 2020/4/7-20:44
-     * @since 2.0.0
+     * @since 1.0.0
      */
     void addTotalThroughput(String name);
 }
